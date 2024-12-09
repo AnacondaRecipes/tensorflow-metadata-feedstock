dc2a7184ccf2110c316042a6dca13fad6680b41e
Steven <scrass@anaconda.com>
2024-12-09 10:38:39 -0700

    update

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
-#!/bin/bash
-
-bazel build tensorflow_metadata:build_pip_package
-bazel run tensorflow_metadata:build_pip_package
-pip install --no-deps dist/*.whl
-=======
 set -ex
 python -m pip install --no-deps --ignore-installed tensorflow_metadata-${PKG_VERSION}-py3-none-any.whl
 # Remove the strict protobuf requirement. Conda will manage it.
 sed -i 's/^Requires-Dist: protobuf.*/Requires-Dist: protobuf/' ${SP_DIR}/tensorflow_metadata-${PKG_VERSION}.dist-info/METADATA
->>>>>>> conda-forge/main
